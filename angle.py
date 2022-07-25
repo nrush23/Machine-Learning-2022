78107fca8b81d10b140040b089a7da702a5e7657
juedrew <109989368+juedrew@users.noreply.github.com>
2022-07-25 16:05:30 -0400

    split into two functions for rad and deg

--- angle.py
+++ angle.py
@@ -1,12 +1,16 @@
 import math
+from re import L
+from telnetlib import X3PAD
 
-def angle(x1,y1,x2,y2,x3,y3):
+def angle_rad(x1,y1,x2,y2,x3,y3):
     # (x1,y1) - one end
     # (x2,y2) - pivot
     # (x3,y3) - other end 
     num = (x1-x2)*(x3-x2)+(y1-y2)*(y3-y2)
     deno = math.sqrt(((x1-x2)**2 + (y1-y2)**2)*((x3-x2)**2 + (y3-y2)**2))
     angle_rad = math.acos(num/deno)
+    return angle_rad
+
+def angle_deg(angle_rad):
     angle_deg = angle_rad*180/math.pi
-    print("angle (rad) = " + str(angle_rad))
-    print("angle (deg) = " + str(angle_deg))
+    return angle_deg
